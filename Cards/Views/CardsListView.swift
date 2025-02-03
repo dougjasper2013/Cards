5606838119f33a02d940695940d0cd20ead643f3
Douglas Jasper <doug.jasper@trios.com>
2025-02-03 10:09:45 -0500

    End of Chapter 13 Plus Challenge

---
+++ Cards/Views/CardsListView.swift
@@ -0,0 +1,38 @@
+//
+//  CardsListView.swift
+//  Cards
+//
+//  Created by Douglas Jasper on 2025-02-03.
+//
+
+import SwiftUI
+
+struct CardsListView: View {
+  @State private var isPresented = false
+
+  var body: some View {
+    list
+    .fullScreenCover(isPresented: $isPresented) {
+      SingleCardView()
+    }
+  }
+
+  var list: some View {
+    ScrollView(showsIndicators: false) {
+      VStack {
+        ForEach(0..<10) { _ in
+          CardThumbnail()
+            .onTapGesture {
+              isPresented = true
+            }
+        }
+      }
+    }
+  }
+}
+
+struct CardsListView_Previews: PreviewProvider {
+  static var previews: some View {
+    CardsListView()
+  }
+}
