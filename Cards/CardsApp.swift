6a40ec70ffb4f958eb351205237983765c09a562
Douglas Jasper <doug.jasper@trios.com>
2025-02-03 08:16:37 -0500

    Initial Commit

---
+++ Cards/CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by Douglas Jasper on 2025-02-03.
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
